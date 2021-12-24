171b77047ab371cceb8c77b212db2d84746db35d
glydokid <glydokidd@naver.com>
2021-12-24 11:22:14 +0900

    Create 14681_사분면 고르기.cpp

---
+++ 단계별 문제풀이/if문/14681_사분면 고르기.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main() {
+	int num1, num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	if ((-1000 <= num1 <= 1000 && num1 != 0) && (-1000 <= num2 <= 1000 && num2 != 0)) {
+		if (num1 > 0 && num2 > 0)
+			printf("1");
+		else if (num1 < 0 && num2 > 0)
+			printf("2");
+		else if (num1 < 0 && num2 < 0)
+			printf("3");
+		else if (num1 > 0 && num2 < 0)
+			printf("4");
+	}
+
+	return 0;
+}
