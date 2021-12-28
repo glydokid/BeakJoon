441e84dbd441b81184cb79e4d9201865d8e3f6f4
glydokid <glydokidd@naver.com>
2021-12-28 16:09:58 +0900

    알람시계

--- 단계별 문제풀이/if문/알람시계_2884.cpp
+++ 단계별 문제풀이/if문/알람시계_2884.cpp
@@ -2,20 +2,20 @@
 int main() {
 	int H, M;
 
-	scanf("%d %d", &H, &M);
+	scanf("%d %d", &H, &M); //시간, 분 입력
 
-	if (H == 0) {
+	if (H == 0) {// 시간이 0시 일 경우
 		if (M < 45) {
 			H = 23;
 			M = M + 60 - 45;
 			printf("%d %d", H, M);
 		}
-		else if (M >= 45) {
+		else if (M >= 45) { 
 			M = M - 45;
 			printf("%d %d", H, M);
 		}
 	}
-	else if (H > 0) {
+	else if (H > 0) { //시간이 0시 보다 클 경우
 		if (M < 45) {
 			H = H - 1;
 			M = M + 60 - 45;
