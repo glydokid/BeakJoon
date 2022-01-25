9417d41a6bf972dfc97eb19ee06fddcd1b373250
glydokid <glydokidd@naver.com>
2022-01-25 14:30:18 +0900

    ACM 호텔

--- 단계별 문제풀이/기초수학1/ACM호텔_10250.cpp
+++ 단계별 문제풀이/기초수학1/ACM호텔_10250.cpp
@@ -1,38 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+	int T;
+	int H, W, N;
 
-	int H, W, N; // 높이, 길이, 순번
-	int T; // 테스트 케이스
+	scanf("%d", &T);
 
-	scanf("%d\n", &T);
-
-	for (int i = 0; i < T; i++) { //테스트 케이스
-
-		int number = 101;
-		int count = 1;
-
+	for (int j = 0; j < T; j++)
+	{
 		scanf("%d %d %d", &H, &W, &N);
 
-		for (int i = 1; i <= W; i++) {
+		if (N % H == 0)
+			printf("%d\n", H * 100 + N / H);
 
-			for (int j = 0; j < H; j++) {
-
-				if (count == N) //순번만큼 반복하면 반복문 나가기
-					goto EXIT;
-
-				number = number + 100; //101호부터 201호...순으로
-
-				if (number / H == 100) { //가장 높은 층에 도착하면 다시 1층부터 시작
-					number = (number / H) + i + 1; 
-				}
-				count++;
-			}
-
-			number = number - 100;
-		}
-	EXIT:
-		printf("%d\n", number);
+		else
+			printf("%d\n", (N % H) * 100 + N / H + 1);
 	}
+
 	return 0;
 }
