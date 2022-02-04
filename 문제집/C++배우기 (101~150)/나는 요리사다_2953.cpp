a037b055d0f1419ff911388b8047bc5a3f37a5fe
glydokid <glydokidd@naver.com>
2022-02-04 15:24:51 +0900

    나는 요리사다

---
+++ 문제집/C++배우기 (101~150)/나는 요리사다_2953.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{	
+	int score = 0;
+	int number = 0;
+	int A[5][5] = {
+		{0, },
+		{0, }
+	};
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			scanf("%d", &A[i][j]);
+			
+			A[i][4] += A[i][j];
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+
+		if (A[i][4] > score) {
+
+			score = A[i][4];
+			number = i+1;
+	
+		}
+	}
+
+	printf("%d %d", number, score);
+}
