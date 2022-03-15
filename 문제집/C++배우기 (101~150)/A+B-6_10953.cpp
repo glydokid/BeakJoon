3c6ed3ec7edfb045df3afc570cc7fc61a55bdfe4
glydokid <glydokidd@naver.com>
2022-03-15 09:07:47 +0900

    A+B -6

---
+++ 문제집/C++배우기 (101~150)/A+B-6_10953.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+	int T = 0;
+	int A = 0, B = 0;
+
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d, %d", &A, &B);
+
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
