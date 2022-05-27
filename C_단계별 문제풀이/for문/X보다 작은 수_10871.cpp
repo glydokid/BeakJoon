ba7cdbc84181671fab2cb8bec87da7e6f8513af7
glydokid <glydokidd@naver.com>
2022-05-27 17:16:42 +0900

    Create X보다 작은 수_10871.cpp

---
+++ C_단계별 문제풀이/for문/X보다 작은 수_10871.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main() {
+    int N, X, input;
+
+    scanf("%d %d", &N, &X); //두 수 입력
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &input);
+
+        if (X > input)
+            printf("%d ", input);
+    }
+    return 0;
+}
