021f235eefb5f268583570ba36c0aa30c7f1413f
glydokid <glydokidd@naver.com>
2022-05-27 17:16:57 +0900

    Create 두 수 비교하기_1330.cpp

---
+++ C_단계별 문제풀이/if문/두 수 비교하기_1330.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main() {
+	int A, B;
+
+	scanf("%d %d", &A, &B); //두 수를 입력
+
+	// 두 수 비교 후 출력
+	if (A > B) {
+		printf(">");
+	}
+	else if (A < B) {
+		printf("<");
+	}
+	else if (A == B) {
+		printf("==");
+	}
+
+	return 0;
+}
