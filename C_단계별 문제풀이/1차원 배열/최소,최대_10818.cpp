a586e15090b8caa53a1c70c5d43308b638377d5c
glydokid <glydokidd@naver.com>
2022-05-27 17:16:29 +0900

    Create 최소,최대_10818.cpp

---
+++ C_단계별 문제풀이/1차원 배열/최소,최대_10818.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+
+	int A, Max = -1000000, Min = 1000000;
+
+	int arr[1000000];
+
+	scanf("%d", &A);
+
+	for (int i = 0; i < A; ++i) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < A; ++i) {
+
+		if (Max < arr[i])
+			Max = arr[i];
+
+		if (Min > arr[i])
+			Min = arr[i];
+	}
+
+	printf("%d %d", Min, Max);
+
+	return 0;
+}
