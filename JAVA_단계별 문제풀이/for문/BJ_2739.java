ef52d4798da99b0473ac56d294891ae5fbc63251
glydokid <glydokidd@naver.com>
2022-05-28 16:13:45 +0900

    구구단

---
+++ JAVA_단계별 문제풀이/for문/BJ_2739.java
@@ -0,0 +1,18 @@
+package BeakJoon_3;
+
+import java.util.Scanner;
+
+public class BJ_2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+
+		sc.close();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(A + " * " + i + " = " + (A * i));
+		}
+	}
+}
