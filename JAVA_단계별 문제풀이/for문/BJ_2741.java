d22c912490c57256bd00fbe7240eff25b43ae5f3
glydokid <glydokidd@naver.com>
2022-05-28 16:14:35 +0900

    N 찍기

---
+++ JAVA_단계별 문제풀이/for문/BJ_2741.java
@@ -0,0 +1,18 @@
+package BeakJoon_3;
+
+import java.util.Scanner;
+
+public class BJ_2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		sc.close();
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(1 + i);
+		}
+	}
+}
