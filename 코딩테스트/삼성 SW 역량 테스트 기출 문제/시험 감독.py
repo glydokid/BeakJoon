9ae31325f979d8440b282b2eec48d660d09f3ad4
glydokid <glydokidd@naver.com>
2022-10-17 12:51:47 +0900

    Create 시험 감독.py

---
+++ 코딩테스트/삼성 SW 역량 테스트 기출 문제/시험 감독.py
@@ -0,0 +1,15 @@
+n = int(input())
+tester = list(map(int, input().split()))
+a, b = map(int, input().split())
+
+cnt = n
+
+for i in tester:
+    i -= a
+    if i > 0:
+        if i % b:
+            cnt += (i//b)+1
+        else:
+            cnt += (i//b)
+
+print(cnt)
